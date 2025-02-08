06dd3af795963d655b5eb4b7c8af729e658ed497
KUKOBA Vadym <zaczacovi4@gmail.com>
2025-02-08 22:50:50 +0100

    exo 2 Des informations manquent !

--- exercices.js
+++ exercices.js
@@ -2,3 +2,10 @@
 
 const h1_element = document.querySelector("h1");
 console.log(h1_element);
+
+// exo 2 Des informations manquent !
+
+const div_description_boutique_element = document.querySelector("#description_boutique");
+const new_description_boutique_element = document.createElement('p');
+new_description_boutique_element.textContent = "Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l'acheter.";
+div_description_boutique_element.appendChild(new_description_boutique_element);
